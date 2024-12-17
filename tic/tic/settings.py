87bbc6794bcbfa0146be78d65e0c37e0cc1e6dc5
LG0625 <161044377+LG0625@users.noreply.github.com>
2024-12-17 00:05:27 +0000

    requiremnts

--- tic/tic/settings.py
+++ tic/tic/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-_txx$5f4o&9+_6x3!12^(i_hy%i2ysho(&$&c%gk7a#i#=lnyz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
